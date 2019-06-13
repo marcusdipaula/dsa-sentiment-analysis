97ebbf80864c9b84aec84f9fe220798a3a0e8da3
Marcus Di Paula <marcusdipaula@gmail.com>
2019-06-13 19:21:09 -0300

    Changes on Project Summary

---
+++ 01-SummaryOfProject.R
@@ -0,0 +1,59 @@
+# Sumário do projeto
+#
+# setwd("F:/FCD/BigDataRAzure/Cap17-20/Mini-Projeto1")
+
+
+### Fundamentos ###
+#
+# 1. Definição de problema e compreensão de contexto
+#
+# 2. Identificação de entidades e atributos
+#
+# 3. Coleta de dados que representem a entidade.
+
+
+### Data Wrangling ###
+#
+# 1. Understand & Tidy the dataset
+#
+# 2. Understand & Analyse: hypothesis generation
+#
+# 3. Understand & transform (Feature Engineering)
+#
+# 4. Data Exploration: hypothesis confirmation
+#
+# 5. Iteration
+
+
+### Data StoryTelling ###
+#
+# 1. Choose appropriate visual
+#
+# 2. Decluter it
+#
+# 3. Focus attention
+#
+# 4. Think like a designer (UX/UI)
+#
+# 5. Tell the story of data
+
+
+# _________________________________Fundamentos____________________________________ #
+#
+# 1.Problema:
+# 2.Contexto:
+# 3.Entidades:
+#
+# ________________________________Data_Wrangling__________________________________ #
+#
+#
+#
+#
+#
+# _______________________________Data_StoryTelling________________________________ #
+#
+#
+#
+#
+#
+# ________________________________________________________________________________ #
